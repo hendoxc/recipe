b0c9dfcfe545e07557bc3730e562c5d08d1f3ffc
hendoxc <hagen.henderson@smartcontract.com>
2024-03-05 12:17:03 +0100

    Adds iceberg-kafka-connect example

---
+++ iceberg/kafka-connect/go.mod
@@ -0,0 +1,20 @@
+module iceberg-kafka-connect
+
+go 1.22.0
+
+require (
+	github.com/hamba/avro v1.8.0
+	github.com/twmb/franz-go v1.16.1
+	github.com/twmb/franz-go/pkg/sr v0.0.0-20240304190937-33923b606f7d
+	golang.org/x/crypto v0.21.0
+)
+
+require (
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.19 // indirect
+	github.com/twmb/franz-go/pkg/kmsg v1.7.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
---
+++ iceberg/kafka-connect/main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/hamba/avro"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/twmb/franz-go/pkg/sr"
+	"golang.org/x/crypto/sha3"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const (
+	BootstrapServers  = "localhost:9092"
+	SchemaRegistryUrl = "http://localhost:8081"
+	topic             = "ethereum.mainnet.blocks"
+	BatchSize         = 10000
+)
+
+type BlockHeader struct {
+	Number     int64  `json:"number" avro:"number"`
+	Hash       string `json:"hash" avro:"hash"`
+	ParentHash string `json:"parent_hash" avro:"parent_hash"`
+	GasUsed    int64  `json:"gas_used" avro:"gas_used"`
+	Timestamp  int64  `json:"timestamp" avro:"timestamp"`
+}
+
+func main() {
+
+	kafkaClient, err := createKafkaClient()
+	if err != nil {
+		panic(err)
+	}
+
+	// register the schema & get a serializer/deserializer (serde)
+	avroSerde, err := registerSchema("block_header.avsc", "ethereum.mainnet.blocks-value")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// each message should
+	prevBlock := BlockHeader{
+		Number:    10000,
+		Timestamp: time.Now().Unix(),
+	}
+
+	var batch []*kgo.Record
+	for i := 1; i <= 1000000; i++ {
+
+		// generate a new block, given the previous block
+		block := generateBlock(prevBlock.Number, prevBlock.Time().Add(time.Duration(30)*time.Second))
+
+		if (i%BatchSize == 0 && len(batch) > 0) || i == 1000000 {
+
+			response := kafkaClient.ProduceSync(context.Background(), batch...)
+			if response.FirstErr() != nil {
+				log.Panic(response.FirstErr())
+			}
+
+			fmt.Printf("Produced %d blocks\n", i)
+			// prob better way to do this
+			batch = []*kgo.Record{}
+		}
+
+		// Note we are using the serde to encode the block to avro
+		batch = append(batch, &kgo.Record{
+			Topic: topic,
+			Key:   []byte(fmt.Sprintf("%d", block.Number)),
+			Value: avroSerde.MustEncode(block),
+		})
+
+	}
+}
+
+func registerSchema(schemaFilePath string, subject string) (*sr.Serde, error) {
+
+	schemaRegistryClient, err := newSchemaRegistryClient()
+	if err != nil {
+		return nil, err
+	}
+
+	// register the schema and Create Serializer/Deserializer
+	serde, err := newSerde[BlockHeader](schemaRegistryClient, schemaFilePath, subject)
+	if err != nil {
+		return nil, err
+	}
+
+	return serde, nil
+}
+
+func createKafkaClient() (*kgo.Client, error) {
+
+	kafkaClient, err := kgo.NewClient(kgo.SeedBrokers(BootstrapServers))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create kafka client %w", err)
+	}
+
+	return kafkaClient, nil
+}
+
+func newSchemaRegistryClient() (*sr.Client, error) {
+
+	schemaRegistryClient, err := sr.NewClient(sr.URLs(SchemaRegistryUrl))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create schema registry client %w", err)
+	}
+
+	return schemaRegistryClient, nil
+}
+
+func newSerde[T any](srClient *sr.Client, schemaFilePath string, subject string) (*sr.Serde, error) {
+
+	schemaTextBytes, err := os.ReadFile(schemaFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read schema file %w", err)
+	}
+
+	subjectSchema, err := srClient.CreateSchema(context.Background(), subject, sr.Schema{
+		Schema: string(schemaTextBytes),
+		Type:   sr.TypeAvro,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to create schema %w", err)
+	}
+
+	avroSchema, err := avro.Parse(string(schemaTextBytes))
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse schema %w", err)
+	}
+
+	var schemaType T
+	var serde sr.Serde
+	serde.Register(
+		subjectSchema.ID,
+		schemaType,
+		sr.EncodeFn(func(a any) ([]byte, error) {
+			return avro.Marshal(avroSchema, a)
+		}),
+		sr.DecodeFn(func(bytes []byte, a any) error {
+			return avro.Unmarshal(avroSchema, bytes, a)
+		}),
+	)
+
+	return &serde, nil
+}
+
+func generateBlock(blockNumber int64, timestamp time.Time) BlockHeader {
+
+	blockHash := sha3.New256()
+	blockHash.Write([]byte(fmt.Sprintf("%d", blockNumber)))
+
+	parentHash := sha3.New256()
+	parentHash.Write([]byte(fmt.Sprintf("%d", blockNumber-1)))
+
+	return BlockHeader{
+		Number:     blockNumber + 1,
+		Hash:       fmt.Sprintf("%x", blockHash.Sum(nil)),
+		ParentHash: fmt.Sprintf("%x", parentHash.Sum(nil)),
+		GasUsed:    rand.Int63n(10000000),
+		Timestamp:  timestamp.Unix(),
+	}
+}
+
+func (b *BlockHeader) String() string {
+	return fmt.Sprintf("Block %d: %s", b.Number, b.Hash)
+}
+
+func (b *BlockHeader) Time() time.Time {
+	return time.Unix(b.Timestamp, 0)
+}
